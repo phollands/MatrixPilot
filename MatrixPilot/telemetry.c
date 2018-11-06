df74d7bdf87d9cdb6bbe5ddf4da7f98ca2ba165f
Peter Hollands <peter.hollands@gmail.com>
2018-11-06 11:37:12 +0000

    telemetry.c: SERIAL_IMU_DUMP: Take out omegacorrP() and replace with rmat6,rmat7.

--- MatrixPilot/telemetry.c
+++ MatrixPilot/telemetry.c
@@ -519,15 +519,15 @@ void telemetry_output_40hz(void)
    }
    else if (wait_time == 1)
    {
-       serial_output("Sx,Sy,Sz,Apx,Apy,Apz,Px,Py,Pz,Ix,Iy,Iz,Wx,Wy,Wz\r\n");
+       serial_output("Sx,Sy,Sz,Apx,Apy,Apz,rmat6,rmat7,Wx,Wy,Wz\r\n");
        wait_time = 0;
    }
    else
    {
-        serial_output("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\r\n",                         \
+        serial_output("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\r\n",                    \
            -aero_force[0], -aero_force[1], -aero_force[2],                          \
            acceleration_plane_x(), acceleration_plane_y(), acceleration_plane_z(),  \
-           omegacorrP[0], omegacorrP[1], omegacorrP[2],                             \
+           rmat[6],rmat[7],                                                         \
            omegagyro[0], omegagyro[1], omegagyro[2]);
    }
 }
