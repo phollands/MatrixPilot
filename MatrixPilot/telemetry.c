20b2f1ed99d2b69d658b2ddac45e13c432cc07b5
Peter Hollands <peter.hollands@gmail.com>
2018-11-06 20:48:42 +0000

    telemetry.c: correction

--- MatrixPilot/telemetry.c
+++ MatrixPilot/telemetry.c
@@ -524,7 +524,7 @@ void telemetry_output_40hz(void)
    }
    else
    {
-        serial_output("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\r\n",                    \
+        serial_output("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\r\n",                    \
            -aero_force[0], -aero_force[1], -aero_force[2],                          \
            acceleration_plane_x(), acceleration_plane_y(), acceleration_plane_z(),  \
            rmat[6],rmat[7],                                                         \
